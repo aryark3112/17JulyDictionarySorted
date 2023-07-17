6bd93da89fe8ee8e6e51565db97594b26bec6fe4
aryark <aryark3112@gmail.com>
2023-07-17 10:03:34 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace _17JulyCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList()
+            { 1,"Sam", 'A',DateTime.Now};
+            Console.WriteLine("item \t Datatype");
+            foreach(var item in arrayList)
+            {
+                Console.WriteLine(item+"\t"+item.GetType());
+            }
+            ArrayList nameList = new ArrayList()
+            { "Raj","Ravi","Amit"};
+            Console.WriteLine("Name List as follows");
+            foreach (var item in nameList)
+            {
+                Console.WriteLine(item);
+            }
+            nameList.Add(12);
+            nameList.Add(DateTime.Now);
+            Console.WriteLine("Name List as Follows");
+            foreach (var item in nameList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
